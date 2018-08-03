e0be51376661030495dbd366e941ad0ff627bdf1
Erin Morrissey <erinmorrissey@gmail.com>
2018-08-02 17:45:10 -0700

    lazy load images for performance purposes

--- js/main.js
+++ js/main.js
@@ -16,6 +16,35 @@ if (navigator.serviceWorker) {
 }
 
 /**
+ * Lazy load images
+ */
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {
+    var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.srcset = lazyImage.dataset.srcset;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      // Possibly fall back to a more compatible method here
+    }
+  }
+}
+
+/**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
@@ -150,8 +179,10 @@ createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
   const image = document.createElement('img');
-  image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.className = 'restaurant-img lazy';
+  image.setAttribute('data-src', DBHelper.imageUrlForRestaurant(restaurant));
+  image.setAttribute('data-srcset', DBHelper.imageUrlForRestaurant(restaurant));
+  image.src = '';
   image.alt = DBHelper.altTagForRestaurant(restaurant);
   li.append(image);
 
